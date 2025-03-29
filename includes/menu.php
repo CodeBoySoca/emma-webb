b9523920e6f1f178b6fd10eebfa381d0cf174182
web-developer-account <rockylatchman3624@gmail.com>
2025-03-29 15:40:10 -0700

    Added README, header and menu includes and some global styles

---
+++ includes/menu.php
@@ -0,0 +1,8 @@
+<nav>
+    <menu>
+        <li><a href="../">About</a></li>
+        <li><a href="../contact.php">Contact</a></li>
+        <li><a href="../achievements.php">Achievements</a></li>
+        <li><a href="../message.php">Leave a message</a></li>
+    </menu>
+</nav>
