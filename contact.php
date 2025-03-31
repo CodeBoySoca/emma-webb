2063c64e105fc8d0d1adacbf2ebbed22fe0a1495
web-developer-account <rockylatchman3624@gmail.com>
2025-03-31 06:39:35 -0700

    HTML and CSS for contact page

--- contact.php
+++ contact.php
@@ -1,5 +1,13 @@
 <?php include 'includes/header.php'; ?>
-
-
-
+<main>
+    <h3>Contact</h3>
+    <form method="post">
+        <input type="text" name="name" id="name" placeholder="Name"><br>
+        <input type="email" name="email" id="email" placeholder="E-mail"><br>
+        <input type="text" name="" id="subject" placeholder="Subject"><br>
+        <textarea name="" id="contact-message" placeholder="Message"></textarea><br>
+        <input type="submit" value="SEND">
+        <input type="text" name="bot-check" id="bot-check" placeholder="2 + 2 =">
+    </form>
+</main>
 <?php include 'includes/footer.php'; ?>
